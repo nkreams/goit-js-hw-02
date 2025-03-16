3fef420bd4126c4476325cbd7ab7d1e9a8fdcca9
Nikita Nemkov <nkreamsov@gmail.com>
2025-03-16 13:48:33 +0200

    rework

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,10 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
+  let totalPrice = quantity * pricePerDroid;
 
   if (totalPrice > customerCredits) {
     return 'Insufficient funds!';
   } else {
-    let remainingCredits = customerCredits - totalPrice;
-    return `You ordered ${quantity} droids worth ${remainingCredits} credits!`;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   }
 }
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
